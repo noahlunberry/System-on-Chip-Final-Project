b35ff3862ca2e040d96a7e7bd8acb441a4968450
Pawin Ruangkanit <ruangkanitpawin@ufl.edu>
2026-03-17 16:29:52 -0400

    updated parser controller

--- rtl/parser_controller.sv
+++ rtl/parser_controller.sv
@@ -1,27 +1,29 @@
-// The parser Controller is an FSM that stores the header data in registers, it
+// The parser Controller is an FSM that stores the HDR1 data in registers, it
 // will decode this into registers to control the enable signals of the FIFO, BRAM and
 // address generators. It will assert done
 
 module parser_controller #(
     parameter int CONFIG_BUS_WIDTH = 64,
-    parameter int PARALLEL_INPUTS = 32,
     parameter int FIFO_RD_WIDTH = 64
 ) (
-    input logic clk,
-    input logic en,
-    input logic rst,
-    input logic [CONFIG_BUS_WIDTH-1:0] data,
-    input logic done
+    input  logic                        clk,
+    input  logic                        valid,
+    input  logic                        rst,
+    input  logic [CONFIG_BUS_WIDTH-1:0] data,
+    input  logic                        empty,
+    output logic                        ready,
+    output logic                        wr_en,
+    output logic                        msg_type,
+    output logic [                 1:0] layer_id,
+    output logic [                31:0] total_bytes
 );
 
-  localparam int PARALLEL_BITS = $clog2(PARALLEL_INPUTS);  // bits needed for batch count
-  //ADD ASSERTION TO BREAK IF PARALLEL_INPUTS IS NOT FACTOR OF 8
-  localparam int PARALLEL_BYTES = (PARALLEL_INPUTS) / 8;
+  localparam logic FIFO_RD_BYTES = (FIFO_RD_WIDTH) / 8;
 
 
   typedef enum logic [1:0] {
-    START,
-    HEADER,
+    HDR0,
+    HDR1,
     PAYLOAD,
     DONE
   } state_t;
@@ -30,86 +32,81 @@ module parser_controller #(
   logic ready_r, next_ready;
 
   state_t state_r, next_state;
-  logic [CONFIG_BUS_WIDTH-1:0] data_r, next_data;
   bit msg_type_r, next_msg_type;
-  logic [7:0] layer_id_r, next_layer_id;
+  logic [1:0] layer_id_r, next_layer_id;
   logic [31:0] total_bytes_r, next_total_bytes;
 
   // Counter Signals
-  logic [15:0] batch_count_r, next_batch_count;
-  logic [15:0] rd_count_r, next_rd_count;
-  logic [31:0] global_count_r, next_global_count;
+  logic [31:0] wr_count_r, next_wr_count;
   logic [31:0] count_r, next_count;
+
+  assign ready = ready_r;
+  assign msg_type = msg_type_r;
+  assign total_bytes = total_bytes_r;
+  assign layer_id = layer_id_r;
 
   always_ff @(posedge clk) begin
     // Control Signals
-    ready_r            <= next_ready;
+    state_r       <= next_state;
+    ready_r       <= next_ready;
 
-    state_r            <= next_state;
-    data_r             <= next_data;
-    msg_type_r         <= next_msg_type;
-    layer_id_r         <= next_layer_id;
-    total_bytes_r      <= next_total_bytes;
-    //counter signals
-    batch_count_r      <= next_batch_count;
-    rd_count_r         <= next_rd_count;
-    global_count_r     <= next_global_count;
-    count_r            <= next_count;
+    msg_type_r    <= next_msg_type;
+    layer_id_r    <= next_layer_id;
+    total_bytes_r <= next_total_bytes;
+    count_r       <= next_count;
+    wr_count_r    <= next_wr_count;
 
     if (rst) begin
-      state_r            <= START;
-      data_r             <= '0;
-      msg_type_r         <= 1'b0;
-      layer_id_r         <= '0;
-      total_bytes_r      <= '0;
+      state_r       <= HDR0;
+      msg_type_r    <= 1'b0;
+      layer_id_r    <= '0;
+      total_bytes_r <= '0;
     end
   end
 
   always_comb begin
     // Control
-    next_ready            = ready_r;
+    next_ready       = ready_r;
 
-    next_state            = state_r;
-    next_data             = data_r;
-    next_msg_type         = msg_type_r;
-    next_layer_id         = layer_id_r;
-    next_total_bytes      = total_bytes_r;
+    next_state       = state_r;
+    next_msg_type    = msg_type_r;
+    next_layer_id    = layer_id_r;
+    next_total_bytes = total_bytes_r;
 
     // Counters
-    next_batch_count      = batch_count_r;
-    next_rd_count         = rd_count_r;
-    next_global_count     = global_count_r;
-    next_count            = count_r;
+    next_wr_count    = wr_count_r;
+    next_count       = count_r;
+
 
 
     case (state_r)
-      START: begin
+      HDR0: begin
         next_ready = 1'b1;
 
-        if (en == 1) begin
+        if (valid == 1) begin
 
-          next_msg_type         = data[0];
-          next_layer_id         = data[15:8];
-          next_state            = HEADER;
+          next_msg_type = data[0];
+          next_layer_id = data[9:8];
+          next_state    = HDR1;
 
-          next_batch_count      = (data[31:16] + PARALLEL_INPUTS - 1) / PARALLEL_INPUTS;
-          next_rd_count         = (data[31:16] + FIFO_RD_WIDTH - 1) / FIFO_RD_WIDTH;
-          next_count            = '0;
+          next_count    = '0;
         end
       end
 
-      HEADER: begin
-        next_state        = PAYLOAD;
-        next_total_bytes  = data[31:0];
-        next_global_count = (data[31:0] + rd_count_r - 1) / rd_count_r;
-
+      HDR1: begin
+        if (valid == 1) begin
+          next_state       = PAYLOAD;
+          next_total_bytes = data[31:0];
+          next_wr_count    = (data[31:0] + FIFO_RD_BYTES - 1) / 8;
+        end
       end
 
       PAYLOAD: begin
-        if (en == 1'b1) begin
+        if (valid == 1'b1) begin
           next_count = count_r + 1'b1;
+          wr_en = 1;
 
-          if (count_r == global_count_r - 1) begin
+          if (count_r == wr_count_r - 1) begin
             next_state = DONE;
             next_ready = 1'b0;
           end
@@ -120,12 +117,11 @@ module parser_controller #(
       // Wait for FIFO to assert done writing the payload to memory, stall reads until then
       // Can be later optimized to improve latency
       DONE: begin
-        if (done) begin
-          next_state = START;
+        if (empty) begin
+          next_state = HDR0;
           next_ready = 1'b1;
         end
       end
-
     endcase
   end
 
