e712ddc49ed0eec50d338990ce33614f17b69d53
paweeny <ruangkanitpawin@ufl.edu>
2026-03-24 00:33:19 -0400

    running pretty fast

--- verification/bnn_fcc_tb.sv
+++ verification/bnn_fcc_tb.sv
@@ -115,7 +115,7 @@ module bnn_fcc_tb #(
     // DUT configuration (can be modified or extended for your own DUT)        
     localparam int NON_INPUT_LAYERS = USE_CUSTOM_TOPOLOGY ? CUSTOM_LAYERS - 1 : TRAINED_LAYERS - 1,
     parameter int PARALLEL_INPUTS = 64,
-    parameter int PARALLEL_NEURONS[NON_INPUT_LAYERS] = '{32, 32, 10}
+    parameter int PARALLEL_NEURONS[NON_INPUT_LAYERS] = '{64, 32, 10}
 );
     import bnn_fcc_tb_pkg::*;
 
