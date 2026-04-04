092ab9e679ff0c03ad824a5597074c95ae9db86a
Pawin Ruangkanit <ruangkanitpawin@ufl.edu>
2026-04-04 15:58:56 -0400

    hopefully fixed makefile

--- verification/bnn_uvm/sources.f
+++ verification/bnn_uvm/sources.f
@@ -1,12 +1,12 @@
-+incdir+../../uvm_reference
++incdir+./uvm_reference
 +incdir+../
 +incdir+.
 +incdir+./env
 +incdir+./seq
 +incdir+./tests
 
-../../uvm_reference/axi4_stream_pkg.sv
-../../uvm_reference/axi4_stream_if.sv
+uvm_reference/axi4_stream_pkg.sv
+uvm_reference/axi4_stream_if.sv
 ../bnn_fcc_tb_pkg.sv
 bnn_fcc_pkg.sv
 tb/bnn_fcc_uvm_tb.sv
